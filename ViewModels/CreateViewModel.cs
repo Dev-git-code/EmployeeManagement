333da0de333bce44b33d1248bea362a2f0e1b082
dev-git-code <devcsgrad@gmail.com>
2024-07-22 11:57:56 +0530

    added password eye button

--- ViewModels/CreateViewModel.cs
+++ ViewModels/CreateViewModel.cs
@@ -10,7 +10,7 @@ namespace EmployeeManagement.ViewModels
         public string? Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password",
             ErrorMessage = "Password and confirmation password do not match.")]
         public string? ConfirmPassword { get; set; }
---
+++ wwwroot/js/passwordToggle.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        if (togglePassword.src.match("https://media.geeksforgeeks.org/wp-content/uploads/20210917150049/eyeslash.png")) {
+            togglePassword.src = "https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png";
+        } else {
+            togglePassword.src = "https://media.geeksforgeeks.org/wp-content/uploads/20210917150049/eyeslash.png";
+        }
+    });
+});
